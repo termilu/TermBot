17d020f0feff7e70bdbcbaa4dbe8aefe12ff07d4
Lucas Rauh <47648082+termilu@users.noreply.github.com>
2020-11-12 19:25:15 +0100

    Initial upload

---
+++ src/termilu/termbot/TermBot.java
@@ -0,0 +1,75 @@
+package termilu.termbot;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+import termilu.termbot.listener.CommandListener;
+
+public class TermBot {
+
+	public JDA jda;
+	public ShardManager shardMan;
+	
+	public static void main(String[] args) {
+		try {
+			new TermBot();
+		} catch (LoginException | IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public TermBot() throws LoginException, IllegalArgumentException  {
+		
+		//JDABuilder builder = JDABuilder.createDefault(null);
+		
+			DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(null);
+			builder.setToken("");
+			builder.setStatus(OnlineStatus.ONLINE);
+			builder.setActivity(Activity.watching("Your Trustfactor :)"));
+			
+			builder.addEventListeners(new CommandListener());
+			
+			shardMan = builder.build();
+			System.out.println("Bot online");
+			
+			shutdown();
+			
+	}
+	
+	public void shutdown() {
+		new Thread(() -> {
+			
+			String line = "";
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			try {
+				while((line = reader.readLine()) != null) {
+					
+					if(line.equalsIgnoreCase("exit")) {
+						if(shardMan != null) {
+							shardMan.setStatus(OnlineStatus.OFFLINE);
+							shardMan.shutdown();
+							System.out.println("Bot offline");
+						}
+						
+						reader.close();
+					}
+					else {
+						System.out.println("Use 'exit' to shutdown.");
+					}
+				}
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}).start();
+	}
+	
+}
---
+++ src/termilu/termbot/listener/CommandListener.java
@@ -0,0 +1,37 @@
+package termilu.termbot.listener;
+
+import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class CommandListener extends ListenerAdapter {
+
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event) {
+		
+		String message = event.getMessage().getContentDisplay();
+		
+		System.out.println(message);
+		
+		if(event.isFromType(ChannelType.TEXT)) {
+			TextChannel channel = event.getTextChannel();
+			
+			//!test arg0 arg1 arg2
+			if(message.startsWith("-")) {
+				String[] args = message.substring(1).split(" ");
+				
+				if(args.length == 1) {
+					if (args[0].equalsIgnoreCase("rep")) {
+						channel.sendMessage("-rep added for " + event.getMember().getAsMention() + "!").queue();
+						
+					}
+				}
+			}
+				
+		}
+		
+		
+	}
+	
+}
