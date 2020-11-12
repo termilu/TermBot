ed6d77e1fec3c402fa60ee479f4b2fd2aa6b0543
Lucas Rauh <47648082+termilu@users.noreply.github.com>
2020-11-12 20:07:06 +0100

    Add files via upload

---
+++ src/termilu/termbot/Config.java
@@ -0,0 +1,12 @@
+package termilu.termbot;
+
+public class Config {
+	
+	public static String Token = "Nzc1Nzk4MTM2MTUyOTE2MDA4.X6rkUQ.QNyRf1O21KDK0FfzehFpKYu0Sfg";
+	
+	public static String getToken() {
+	      return Token;
+	   }
+	
+	
+}
